866b9a723b5f3d0e36f1f35c9c468e9495a354c5
Sumit Jangir <sumitjangir.jecrc@gmail.com>
2021-06-22 21:30:00 +0530

    fix side nav issue

--- js/init.js
+++ js/init.js
@@ -1,13 +1,7 @@
-(function ($) {
-  $(function () {
-    $(".sidenav").sidenav();
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
-
 $(document).ready(function () {
-  $(".modal").modal();
   $(".page-nav-bar").html(getNavBar());
   $(".page-footer").html(getFooterBar());
+  $(".sidenav").sidenav();
 });
 
 function getNavBar() {
